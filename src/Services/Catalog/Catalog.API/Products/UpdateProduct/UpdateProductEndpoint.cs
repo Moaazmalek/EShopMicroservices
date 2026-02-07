86ee5b20c6d2e44bea7d4555150ea2e0726eee0d
moaaz malek <madhalfrwat@gmail.com>
2026-02-07 11:56:08 +0300

    Add Update Product Features and Test it by Postman.

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest(
+        Guid Id,
+        string Name,
+        List<string> Category,
+        decimal Price,
+        string Description,
+        string ImageFile);
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            })
+             .WithName("UpdateProduct")
+             .WithDescription("Update product")
+             .WithSummary("Update Product")
+             .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .ProducesProblem(StatusCodes.Status404NotFound);
+        }
+    }
+}
