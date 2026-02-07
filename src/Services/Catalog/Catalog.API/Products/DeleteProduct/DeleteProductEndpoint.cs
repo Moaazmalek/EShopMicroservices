c06fb2b4c3b834597ba9163884414df63392ee77
moaaz malek <madhalfrwat@gmail.com>
2026-02-07 12:19:33 +0300

    Add Delete Product Feature and test it.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+           app.MapDelete("/products/{id:guid}", async (ISender sender, Guid id) =>
+            {
+                var command = new DeleteProductCommand(id);
+                var result = await sender.Send(command);
+                var response = result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .WithDescription("Delete a product from the catalog")
+            .WithSummary("Delete Product")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound);
+        }
+    }
+}
