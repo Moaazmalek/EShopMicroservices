ef9c041a448f5949ac5e5324dafebdb44ae0bef5
moaaz malek <madhalfrwat@gmail.com>
2026-02-09 17:26:54 +0300

    Add Repository Pattern to work with Marten, Postgresql, and Redis

---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        //define contracts 
+
+       Task<ShoppingCart> GetBasket(string userName,CancellationToken cancellation=default);
+        Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default);
+
+        Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
+
+
+    }
+}
