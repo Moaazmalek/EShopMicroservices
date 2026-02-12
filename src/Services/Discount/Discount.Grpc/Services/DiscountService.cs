f5fb9cc36098ddff5c0eb90244f928c43003d9e8
moaaz malek <madhalfrwat@gmail.com>
2026-02-12 10:54:08 +0300

    Implementing serviecs from DiscountProtoServiceBase and Configure it in Program.cs with app.MapGrpcService<DiscountService>()

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,7 +1,30 @@
-﻿namespace Discount.Grpc.Services
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services
 {
-    public class DiscountService
+    public class DiscountService:DiscountProtoService.DiscountProtoServiceBase
     {
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            // TODO: Get Discount from database
+            return base.GetDiscount(request, context);
+        }
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            // TODO: Create Discount in database
+            return base.CreateDiscount(request, context);
+        }
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            // TODO: Update Discount in database
+            return base.UpdateDiscount(request, context);
+        }
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            // TODO: Delete Discount from database
+            return base.DeleteDiscount(request, context);
+        }
+
 
     }
 }
