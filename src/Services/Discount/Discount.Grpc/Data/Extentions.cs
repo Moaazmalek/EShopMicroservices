bc7467669079f8f8ef09d04d5266971730f4f7d6
moaaz malek <madhalfrwat@gmail.com>
2026-02-12 12:46:31 +0300

    Add Extention UseMigration method because of the containerization on docker we can't run the command or using CLI so automigration. and seeding data on Coupon table.

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+       public static async Task<IApplicationBuilder> UseMigration(this IApplicationBuilder app)
+        {
+            // Create a scope to get the DbContext instance
+            using var scope= app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountDbContext>();
+
+             await dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,3 @@
-
 using Discount.Grpc.Data;
 using Discount.Grpc.Services;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +11,7 @@ builder.Services.AddDbContext<DiscountDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+await app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
