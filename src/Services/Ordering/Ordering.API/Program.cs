96fabf36efca797f928e3d101a3586f467eb33bd
moaaz malek <madhalfrwat@gmail.com>
2026-02-14 00:21:32 +0300

    Add Dependencies between Layers and Configure Dependency Injection in each layer and Configure the Ordering.API Program.cs with all Extentions.

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,22 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container.
+//---------------------
+//Infrastructure - EF Core
+//Application - MediatR
+//API - Carter, HealthChecks, ..
+//----------------
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureService(builder.Configuration)
+    .AddApiServices();
+    
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
