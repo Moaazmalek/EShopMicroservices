1a55b249864fd06f18ae307a971b71461d0d5ded
moaaz malek <madhalfrwat@gmail.com>
2026-02-13 17:06:53 +0300

    Fix SSL Certification Exception. in Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -40,7 +40,13 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
     options =>
     {
         options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
-    });
+    }).ConfigurePrimaryHttpMessageHandler(() => {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback=HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+        return handler;
+        });
 
 
 
