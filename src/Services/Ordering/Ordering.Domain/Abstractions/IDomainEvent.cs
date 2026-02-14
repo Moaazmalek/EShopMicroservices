533542f8a0701a8c1b477b90cbe96c6cb89c5da2
moaaz malek <madhalfrwat@gmail.com>
2026-02-14 16:01:27 +0300

    Develop DDD Abstractions (Entity, Domain Event, Aggregate ).

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public interface IDomainEvent : INotification
+    {
+        // Expression-bodied read-only property 
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
