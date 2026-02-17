83433110b23d3606e97baedb9657c7da1b7dad3b
moaaz malek <madhalfrwat@gmail.com>
2026-02-17 14:48:55 +0300

    Configure db entities

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
