971eddfeb3294a72f68ab1cce97698effe84ec04
moaaz malek <madhalfrwat@gmail.com>
2026-02-24 03:49:27 +0300

    Develop Dto classes

---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Ordering.Application.Dtos
+{
+    public record PaymentDto(
+        string CardName,
+        string CardNumber,
+        string Expiration , 
+        string  Cvv,
+        int PaymentMethod
+        );
+    
+}
