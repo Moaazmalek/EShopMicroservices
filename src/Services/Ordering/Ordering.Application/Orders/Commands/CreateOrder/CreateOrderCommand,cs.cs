85586cdf7735219394aa673be9e8220f532ab201
moaaz malek <madhalfrwat@gmail.com>
2026-02-24 04:06:40 +0300

    Develop Create Order Command and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand,cs.cs
@@ -0,0 +1,24 @@
+﻿
+using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(x => x.Order.OorderName).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required.");
+            RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be null.");
+            
+
+        }
+    }
+    public record CreateOrderResult(Guid Id);
+    public record CreateOrderCommand(OrderDto Order) :
+        ICommand<CreateOrderResult>;
+
+    
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler :
+        ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            //Create order entity from command object
+            //save to database
+            //return result
+            
+        }
+    }
+}
