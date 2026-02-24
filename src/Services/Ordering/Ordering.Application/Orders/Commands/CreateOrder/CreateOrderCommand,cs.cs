21c54fe7599e1734d7743cd0d3f746a2f22cf74f
moaaz malek <madhalfrwat@gmail.com>
2026-02-24 17:24:35 +0300

    Fix Syntax Error on Create Order Command

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand,cs.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand,cs.cs
@@ -9,7 +9,7 @@ namespace Ordering.Application.Orders.Commands.CreateOrder
     {
         public CreateOrderCommandValidator()
         {
-            RuleFor(x => x.Order.OorderName).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Name is required.");
             RuleFor(x => x.Order.CustomerId).NotEmpty().WithMessage("CustomerId is required.");
             RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be null.");
             
