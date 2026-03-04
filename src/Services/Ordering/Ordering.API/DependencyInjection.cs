f977e7a63463e8c32cb095c1b85f629da34415c5
moaaz malek <madhalfrwat@gmail.com>
2026-03-04 06:28:59 +0300

    Adding CustomExceptionHandler for standardized formating error in Ordering.API DependencyInjection ApiServices .

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Carter;
+﻿using BuildingBlocks.Exceptions.Handler;
+using Carter;
 
 namespace Ordering.API
 {
@@ -7,12 +8,14 @@ namespace Ordering.API
         public static IServiceCollection AddApiServices(this IServiceCollection services)
         {
             services.AddCarter();
+            services.AddExceptionHandler<CustomExceptionHandler>();
 
             return services;
         }
         public static WebApplication UseApiServices(this WebApplication app)
         {
             app.MapCarter();
+            app.UseExceptionHandler(options => { });
             return app;
         }
 
