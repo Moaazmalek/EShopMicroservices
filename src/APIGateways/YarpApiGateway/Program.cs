1442c57b52def0a83c133ecc119dba0f544de6f4
moaaz malek <madhalfrwat@gmail.com>
2026-03-10 08:04:49 +0300

    Conainerize and Orchestrate YarpApiGateway

--- src/APIGateways/YarpApiGateway/Program.cs
+++ src/APIGateways/YarpApiGateway/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    
 builder.Services.AddRateLimiter( rateLimiterOptions =>
 {
     rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 //API - Carter, HealthChecks, ..
 //----------------
 
+
 builder.Services
     .AddApplicationServices(builder.Configuration)
     .AddInfrastructureService(builder.Configuration)
