4af36537c43dddf15e0e3b2ed96cadbcd40544dc
moaaz malek <madhalfrwat@gmail.com>
2026-03-12 03:44:54 +0300

    Create Catalog Models for Shopping Web Application Microservices consume.

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Models.Catalog
+{
+    public class ProductModel
+    {  
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = string.Empty;
+        public List<string> Category { get; set; } = [];
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+    public record GetProductsResponse(IEnumerable<ProductModel> Products);
+    public record GetProductByCategoryResponse(IEnumerable<ProductModel> Products);
+    public record GetProductByIdResponse(ProductModel Product);
+}
