665e379ec88125504b343f316907fd062bcadb6e
moaaz malek <madhalfrwat@gmail.com>
2026-03-14 06:34:24 +0300

    Develop Index html pages

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,10 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Shopping.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<ProductModel> ProductList { get; set; }=[];
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
 
